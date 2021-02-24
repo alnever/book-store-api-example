be2e71ea4f266828f61a0188a604291c71ac7feb
out-motus-neverov-a <diK6-jdye-Rl73>
2021-02-24 15:56:00 +0500

    Initial commit

---
+++ src/main/java/com/example/demo/model/Book.java
@@ -0,0 +1,63 @@
+package com.example.demo.model;
+
+import javax.persistence.*;
+
+@Entity
+@SequenceGenerator(name="bookIdGenerator", initialValue = 1)
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "bookIdGenerator")
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column
+    private String author;
+
+    @Column
+    private Double price;
+
+
+    public Book() {
+    }
+
+    public Book(String title, String author, Double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
